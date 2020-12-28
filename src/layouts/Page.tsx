b135aeb94275037bc7224269551073c57018852f
Jake Runzer <jakerunzer@gmail.com>
2020-12-28 16:01:13 -0700

    update layout

--- src/layouts/Page.tsx
+++ src/layouts/Page.tsx
@@ -12,11 +12,11 @@ export const Page: React.FC<Props> = props => {
     <>
       <SEO {...props.seo} />
 
-      <div className="flex flex-col max-w-7xl w-full px-4 mx-auto">
+      <div className="flex flex-col max-w-5xl w-full px-4 mx-auto">
         <div className="min-h-screen">
           <Nav />
 
-          <main className="flex-grow max-w-4xl pb-12 w-full mx-auto">
+          <main className="flex-grow pb-12 w-full mx-auto">
             {props.children}
           </main>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import { appStoreUrl } from "../constants";
 const Home: NextPage = () => {
   return (
     <Page>
-      <section className="flex px-5 py-12 md:flex-row flex-col items-center">
+      <section className="flex px-5 py-12 md:flex-row flex-col items-center justify-between">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col items-center md:items-start text-center md:text-left mb-16 md:mb-0">
           <h1 className="sm:text-5xl text-4xl mb-4 font-extrabold text-gray-900">
             Bowtie
